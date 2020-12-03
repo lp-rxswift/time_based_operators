b006c4193ee3a049784d8283b837fa50dc68c0ff
Lucas <lucaspedrazoli@gmail.com>
2020-12-03 16:10:35 -0300

    add windowed buffer example

--- RxSwiftPlayground.playground/Pages/window.xcplaygroundpage/Contents.swift
+++ RxSwiftPlayground.playground/Pages/window.xcplaygroundpage/Contents.swift
@@ -4,7 +4,50 @@ import RxCocoa
 
 
 // Start coding here
+let elementsPerSecond = 3
+let windowTimeSpan: RxTimeInterval = .seconds(4)
+let windowMaxCount = 10
+let sourceObservable = PublishSubject<String>()
 
+let sourceTimeline = TimelineView<String>.make()
+
+let stack = UIStackView.makeVertical([
+  UILabel.makeTitle("window"),
+  UILabel.make("Emitted elements (\(elementsPerSecond) persec.):"),
+  sourceTimeline,
+  UILabel.make("Windowed observables (at most \(windowMaxCount) every \(windowTimeSpan) sec):")])
+
+let timer = DispatchSource.timer(interval: 1.0 /
+Double(elementsPerSecond), queue: .main) {
+  sourceObservable.onNext("🐱")
+}
+
+_ = sourceObservable.subscribe(sourceTimeline)
+
+_ = sourceObservable
+  .window(timeSpan: windowTimeSpan,
+                            count: windowMaxCount,
+                            scheduler: MainScheduler.instance)
+  .flatMap { windowedObservable -> Observable<(TimelineView<Int>, String?)> in
+    let timeline = TimelineView<Int>.make()
+    stack.insert(timeline, at: 4)
+    stack.keep(atMost: 8)
+    return windowedObservable
+      .map { value in (timeline, value) }
+      .concat(Observable.just((timeline, nil)))
+  }
+  .subscribe(onNext: { tuple in
+    let (timeline, value) = tuple
+    if let value = value {
+      timeline.add(.next(value))
+    } else {
+      timeline.add(.completed(true))
+    }
+  })
+
+let hostView = setupHostView()
+hostView.addSubview(stack)
+hostView
 
 
 // Support code -- DO NOT REMOVE
